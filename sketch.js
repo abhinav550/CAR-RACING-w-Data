5cb3c1d06dad21c236da3529d204fb7a35eadae8
abhinav550 <maabhinav550@gmail.com>
2020-04-26 16:10:35 +0530

    multiplayer

--- sketch.js
+++ sketch.js
@@ -1,13 +1,18 @@
 var ball;
+var database,ballrefpos,position;
 
 function setup(){
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    database = firebase.database();
+    ballrefpos = database.ref('ball/position');
+    ballrefpos.on("value",ballposition)
 }
 
 function draw(){
     background("white");
+    if (position!==undefined) 
     if(keyDown(LEFT_ARROW)){
         changePosition(-1,0);
     }
@@ -20,10 +25,22 @@ function draw(){
     else if(keyDown(DOWN_ARROW)){
         changePosition(0,+1);
     }
+
     drawSprites();
+}
+function ballposition(data){
+position=data.val();
+ball.x=position.x;
+ball.y=position.y;
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    database.ref('ball/position').set({
+        'x':position.x+x,
+        'y':position.y+y
+    }
+
+    )
+
+   
 }
